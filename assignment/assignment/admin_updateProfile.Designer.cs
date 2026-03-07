7b42b9ab783509b6888ae2c40d89410801b35328
Cathryne <cathrynepang@gmail.com>
2026-03-07 11:19:56 +0800

    add update profile page

---
+++ assignment/assignment/admin_updateProfile.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace assignment
+{
+    partial class admin_updateProfile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            lblDelete = new Label();
+            lblUsername = new Label();
+            lblPw = new Label();
+            lblEmail = new Label();
+            lblContact = new Label();
+            lblAddress = new Label();
+            label6 = new Label();
+            btnChangeUsername = new Button();
+            btnChangePw = new Button();
+            btnChangeEmail = new Button();
+            btnChangeContact = new Button();
+            btnChangeAddress = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11F);
+            label1.Location = new Point(57, 95);
+            label1.Name = "label1";
+            label1.Size = new Size(111, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Username";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11F);
+            label2.Location = new Point(57, 156);
+            label2.Name = "label2";
+            label2.Size = new Size(103, 30);
+            label2.TabIndex = 1;
+            label2.Text = "Password";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11F);
+            label3.Location = new Point(57, 219);
+            label3.Name = "label3";
+            label3.Size = new Size(64, 30);
+            label3.TabIndex = 2;
+            label3.Text = "Email";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 11F);
+            label4.Location = new Point(57, 284);
+            label4.Name = "label4";
+            label4.Size = new Size(173, 30);
+            label4.TabIndex = 3;
+            label4.Text = "Contact Number";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 11F);
+            label5.Location = new Point(57, 347);
+            label5.Name = "label5";
+            label5.Size = new Size(91, 30);
+            label5.TabIndex = 4;
+            label5.Text = "Address";
+            // 
+            // lblDelete
+            // 
+            lblDelete.AutoSize = true;
+            lblDelete.Font = new Font("Segoe UI", 11F);
+            lblDelete.Location = new Point(500, 411);
+            lblDelete.Name = "lblDelete";
+            lblDelete.Size = new Size(181, 30);
+            lblDelete.TabIndex = 5;
+            lblDelete.Text = "Delete Account >";
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.BorderStyle = BorderStyle.Fixed3D;
+            lblUsername.Font = new Font("Segoe UI", 11F);
+            lblUsername.Location = new Point(236, 95);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(2, 32);
+            lblUsername.TabIndex = 6;
+            // 
+            // lblPw
+            // 
+            lblPw.AutoSize = true;
+            lblPw.BorderStyle = BorderStyle.Fixed3D;
+            lblPw.Font = new Font("Segoe UI", 11F);
+            lblPw.Location = new Point(236, 156);
+            lblPw.Name = "lblPw";
+            lblPw.Size = new Size(2, 32);
+            lblPw.TabIndex = 7;
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.BorderStyle = BorderStyle.Fixed3D;
+            lblEmail.Font = new Font("Segoe UI", 11F);
+            lblEmail.Location = new Point(236, 219);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(2, 32);
+            lblEmail.TabIndex = 8;
+            // 
+            // lblContact
+            // 
+            lblContact.AutoSize = true;
+            lblContact.BorderStyle = BorderStyle.Fixed3D;
+            lblContact.Font = new Font("Segoe UI", 11F);
+            lblContact.Location = new Point(236, 284);
+            lblContact.Name = "lblContact";
+            lblContact.Size = new Size(2, 32);
+            lblContact.TabIndex = 9;
+            // 
+            // lblAddress
+            // 
+            lblAddress.AutoSize = true;
+            lblAddress.BorderStyle = BorderStyle.Fixed3D;
+            lblAddress.Font = new Font("Segoe UI", 11F);
+            lblAddress.Location = new Point(236, 347);
+            lblAddress.Name = "lblAddress";
+            lblAddress.Size = new Size(2, 32);
+            lblAddress.TabIndex = 10;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 18F);
+            label6.Location = new Point(276, 22);
+            label6.Name = "label6";
+            label6.Size = new Size(122, 48);
+            label6.TabIndex = 11;
+            label6.Text = "Profile";
+            // 
+            // btnChangeUsername
+            // 
+            btnChangeUsername.Location = new Point(519, 94);
+            btnChangeUsername.Name = "btnChangeUsername";
+            btnChangeUsername.Size = new Size(112, 34);
+            btnChangeUsername.TabIndex = 12;
+            btnChangeUsername.Text = "Change";
+            btnChangeUsername.UseVisualStyleBackColor = true;
+            // 
+            // btnChangePw
+            // 
+            btnChangePw.Location = new Point(519, 155);
+            btnChangePw.Name = "btnChangePw";
+            btnChangePw.Size = new Size(112, 34);
+            btnChangePw.TabIndex = 13;
+            btnChangePw.Text = "Change";
+            btnChangePw.UseVisualStyleBackColor = true;
+            // 
+            // btnChangeEmail
+            // 
+            btnChangeEmail.Location = new Point(519, 218);
+            btnChangeEmail.Name = "btnChangeEmail";
+            btnChangeEmail.Size = new Size(112, 34);
+            btnChangeEmail.TabIndex = 14;
+            btnChangeEmail.Text = "Change";
+            btnChangeEmail.UseVisualStyleBackColor = true;
+            // 
+            // btnChangeContact
+            // 
+            btnChangeContact.Location = new Point(519, 283);
+            btnChangeContact.Name = "btnChangeContact";
+            btnChangeContact.Size = new Size(112, 34);
+            btnChangeContact.TabIndex = 15;
+            btnChangeContact.Text = "Change";
+            btnChangeContact.UseVisualStyleBackColor = true;
+            // 
+            // btnChangeAddress
+            // 
+            btnChangeAddress.Location = new Point(519, 346);
+            btnChangeAddress.Name = "btnChangeAddress";
+            btnChangeAddress.Size = new Size(112, 34);
+            btnChangeAddress.TabIndex = 16;
+            btnChangeAddress.Text = "Change";
+            btnChangeAddress.UseVisualStyleBackColor = true;
+            // 
+            // admin_updateProfile
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(693, 450);
+            Controls.Add(btnChangeAddress);
+            Controls.Add(btnChangeContact);
+            Controls.Add(btnChangeEmail);
+            Controls.Add(btnChangePw);
+            Controls.Add(btnChangeUsername);
+            Controls.Add(label6);
+            Controls.Add(lblAddress);
+            Controls.Add(lblContact);
+            Controls.Add(lblEmail);
+            Controls.Add(lblPw);
+            Controls.Add(lblUsername);
+            Controls.Add(lblDelete);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "admin_updateProfile";
+            Text = "updateProfile";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label lblDelete;
+        private Label lblUsername;
+        private Label lblPw;
+        private Label lblEmail;
+        private Label lblContact;
+        private Label lblAddress;
+        private Label label6;
+        private Button btnChangeUsername;
+        private Button btnChangePw;
+        private Button btnChangeEmail;
+        private Button btnChangeContact;
+        private Button btnChangeAddress;
+    }
+}
